cb897c211a26706c76dde66c439459523c41dbce
Chris Howe-Jones <chris.howejones@gmail.com>
2024-04-28 17:06:24 +0100

    Added reducer example.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
-import Counter from "./component/UseStatePractice.js/Counter";
-import Counter2 from "./component/UseStatePractice.js/Counter2";
-import Counter3 from "./component/UseStatePractice.js/Counter3";
-import HookStateArray from "./component/UseStatePractice.js/HookStateArray";
-import EffectExample1 from "./component/UseEffectPractice/EffectExample1";
-import EffectExample2 from "./component/UseEffectPractice/EffectExample2";
-import EffectExample3 from "./component/UseEffectPractice/EffectExample3";
-import MouseContainer from "./component/UseEffectPractice/MouseContainer";
-import FetchMultiplePosts from "./component/FetchMultiplePosts";
+// import Counter from "./component/UseStatePractice.js/Counter";
+// import Counter2 from "./component/UseStatePractice.js/Counter2";
+// import Counter3 from "./component/UseStatePractice.js/Counter3";
+// import HookStateArray from "./component/UseStatePractice.js/HookStateArray";
+// import EffectExample1 from "./component/UseEffectPractice/EffectExample1";
+// import EffectExample2 from "./component/UseEffectPractice/EffectExample2";
+// import EffectExample3 from "./component/UseEffectPractice/EffectExample3";
+// import MouseContainer from "./component/UseEffectPractice/MouseContainer";
+// import FetchMultiplePosts from "./component/FetchMultiplePosts";
+import MyCounter1 from "./component/useReducerPractice/MyCounter1";
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
              <HookStateArray />
        */}
       {/* */}
-      <FetchMultiplePosts />
+      <MyCounter1 />
     </div>
   );
 }
